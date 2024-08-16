79f46369343ef84d2e0c51ac053158968309f34c
Nikhil <nikhil@tartanhq.com>
2024-08-16 18:44:34 +0530

    first commit

--- src/AgentBox.tsx
+++ src/AgentBox.tsx
@@ -1,5 +1,5 @@
 export default function AgentBox() {
   return (
-    <div className="flex justify-center items-center w-full md:w-[232px] h-[330px] bg-secondary rounded-lg border-2 border-secondary-light transition-transform transform hover:scale-105 hover:shadow-lg duration-300 ease-in-out"></div>
+    <div className="flex justify-center items-center w-full md:w-[232px] h-[330px] bg-secondary-light rounded-lg border-2 border-secondary-dark transition-transform transform hover:scale-105 hover:shadow-lg duration-300 ease-in-out"></div>
   );
 }
